fda85aeec31b27ea4952543cf8a480430559adfd
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-05-16 20:11:11 +0300

    feat: add user interaction with cell

---
+++ minesweeper/src/js/cells.js
@@ -0,0 +1,42 @@
+function openCell(cell) {
+  if (Number(cell.innerText) === 0) {cell.classList.add('empty')};
+  if (Number(cell.innerText) === 1) {cell.classList.add('cell-1')};
+  if (Number(cell.innerText) === 2) {cell.classList.add('cell-2')};
+  if (Number(cell.innerText) === 3) {cell.classList.add('cell-3')};
+  if (Number(cell.innerText) === 4) {cell.classList.add('cell-4')};
+  if (Number(cell.innerText) === 5) {cell.classList.add('cell-5')};
+  if (Number(cell.innerText) === 6) {cell.classList.add('cell-6')};
+  if (Number(cell.innerText) === 7) {cell.classList.add('cell-7')};
+  if (Number(cell.innerText) === 8) {cell.classList.add('cell-8')};
+  if (cell.innerText === '*') {cell.classList.add('mine')};
+}
+
+function changeState(cell) {
+  if (cell.matches('.flag')) {
+    cell.classList.toggle('flag');
+    cell.classList.toggle('question');
+  } else if (cell.matches('.question')) {
+    cell.classList.toggle('question');
+  } else {
+    cell.classList.toggle('flag');
+  }
+  console.log(cell);
+}
+
+function getCellCoordinates(value) {
+  const i = Math.floor(Number(value) / fieldSize);
+  const j = Number(value) % fieldSize;
+  return [i, j];
+}
+  
+addEventListener('click', event => {
+  if (event.target.className === 'cell') {
+    openCell(event.target);
+  }
+})
+
+addEventListener('contextmenu', event => {
+  if (event.target.matches('.cell')) {
+    changeState(event.target);
+  }
+})
--- minesweeper/src/js/index.js
+++ minesweeper/src/js/index.js
@@ -1,28 +1,15 @@
 import './field.js';
 import './mines.js';
+import './cells.js';
 
-function openCell(cell) {
-  console.log(cell);
-  if (Number(cell.innerText) === 0) {cell.classList.add('empty')};
-  if (Number(cell.innerText) === 1) {cell.classList.add('cell-1')};
-
-}
-
-function getCellCoordinates(value) {
-  const i = Math.floor(Number(value) / fieldSize);
-  const j = Number(value) % fieldSize;
-  return [i, j];
-}
-
+addEventListener('mousedown', event => {
+  event.preventDefault();
+})
 
 addEventListener('click', event => {
   event.preventDefault();
-  if (event.target.className === 'cell') {
-    openCell(event.target);
-  }
 })
 
-
-addEventListener('mousedown', event => {
+addEventListener('contextmenu', event => {
   event.preventDefault();
 })
