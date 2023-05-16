300533e5909ec3b3bb6bfea7c5a338c01fe5c6b8
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-05-16 14:44:43 +0300

    feat: create game field depend on selected size

--- minesweeper/src/js/index.js
+++ minesweeper/src/js/index.js
@@ -1,3 +1,30 @@
 import './hello.js';
 
-console.log('Hello!');
+const fieldSize = 15;
+
+const field = document.createElement('div');
+field.className = 'field';
+document.body.append(field);
+
+class Cell {
+  constructor(num) {
+    this.elem = document.createElement('div');
+    this.elem.className = 'cell';
+    this.elem.setAttribute('num', `${num}`);
+  }
+}
+
+function createField() {
+  for (let i = 0; i < fieldSize; i += 1) {
+    const cellRow = document.createElement('div');
+    cellRow.className = 'cell-row';
+    field.append(cellRow);
+    for (let j = 0; j < fieldSize; j += 1) {
+      const cellNumber = i * fieldSize + j;
+      const newCell = new Cell(cellNumber);
+      cellRow.append(newCell.elem);
+    }
+  }
+}
+
+createField();
